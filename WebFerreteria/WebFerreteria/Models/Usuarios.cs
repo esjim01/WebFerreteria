6a94f2e9aa9d672363acde6e415211e14854e5db
esjim01 <eduar.jimenez45164@ucaldas.edu.co>
2025-04-30 19:25:52 -0500

    Prueba de commit

--- WebFerreteria/WebFerreteria/Models/Usuarios.cs
+++ WebFerreteria/WebFerreteria/Models/Usuarios.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace WebFerreteria.Models;
 
@@ -10,34 +9,13 @@ public partial class Usuarios
 
     public string? NombreCompleto { get; set; }
 
-    [Required]
-    [EmailAddress]
     public string? Correo { get; set; }
 
-    [Required]
-    [DataType(DataType.Password)]
     public string? Contrasena { get; set; }
 
-    [Required]
-    public string? Rol { get; set; } // "admin", "empleado", "ventas", etc.
+    public string? Rol { get; set; }
 
     public bool? Estado { get; set; }
 
     public virtual ICollection<Entregas> Entregas { get; set; } = new List<Entregas>();
-
-    public class LoginModel
-    {
-        public InputModel Input { get; set; }
-
-        public class InputModel
-        {
-            [Required(ErrorMessage = "El correo electrónico es obligatorio")]
-            [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
-            public string Correo { get; set; }
-
-            [Required(ErrorMessage = "La contraseña es obligatoria")]
-            [DataType(DataType.Password)]
-            public string Contrasena { get; set; }
-        }
-    }
 }
